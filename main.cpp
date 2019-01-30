91d4edf33f5351eeb938719f98e40a6355fa5d57
Claudia Bodea <bodea.claudia.maria@gmail.com>
2019-01-30 22:57:40 +0200

    Exercitiul 4

--- main.cpp
+++ main.cpp
@@ -3,13 +3,27 @@
 using namespace std;
 
 int main(){
-   int a[20], i;
-   for (i = 0; i < 20; i++) {
-      a[i] = 19 - i;
-   }
-   for (i = 0; i < 20; i++) {
-      cout << a[i] << " ";
-   }
-   cout << endl;
+   int a[10], i, nr;
+   cout << "cate nr doriti? " << endl;
+   cin >> nr;
+
+   for (i = 0; i < nr; i++) {
+        cout << "a["<< i <<"]: ";
+         cin >> a[i];
+    }
+
+
+
+      for (i = 0; i < nr; i++) {
+        if (a[i] < 0)
+          a[i] = - a[i];
+      }
+
+
+     for (i = 0; i < nr; i++) {
+        cout << a[i] << ", ";
+    }
+    cout << endl;
+
    return 0;
 }
